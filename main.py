84a6b1ea3d01c78ca3eabc360c86bfad8e788f45
respondnet <respondnet@users.noreply.github.com>
2024-01-26 15:26:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from gevent.pywsgi import WSGIServer
 from flask import Flask, request
 from os import getenv, getcwd, listdir
+from os.path import join
 
 # 初始化 (固定格式)
 app = Flask(__name__)
@@ -12,8 +13,9 @@ def hello():
     if request.method == "GET":
         # get通过request.args.get("param_name","")形式获取参数值
 
+        dir = getcwd()
         files = [join(dir, file) for file in listdir(dir)]
-        
+
         return {'code':200, 'message':'success', 'data':{"curdir":getcwd(),"files:":str(files)}}
     elif request.method == "POST":
         name = request.form["name"]
